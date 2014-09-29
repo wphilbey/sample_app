ff4ce028a9bac5220f6a263d789d197cd4a48dc3
Warren Philbey <wphilbey@hyperneph.com>
2014-09-29 12:46:39 +0100

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Including Capybara required to run integration tests
+  config.include Capybara::DSL
 end
