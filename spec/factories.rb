5748b1efa5390aaf13d7b2a94fd6d78c8ee27f94
Warren Philbey <wphilbey@hyperneph.com>
2014-10-02 12:37:07 +0100

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Example User"
+    email    "user@hyperneph.com"
+    password "Welcome100"
+    password_confirmation "Welcome100"
+  end
+end
