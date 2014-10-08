768162760ea28f7c73073734fea98db01fdfde3c
Warren Philbey <wphilbey@hyperneph.com>
2014-10-08 15:01:41 +0100

    Finish chapter 8 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -38,12 +38,7 @@ describe "User pages" do
   	end
 
   	describe "with valid information" do
-  		before do
-  			fill_in "Name", with: "Example User"
-  			fill_in "Email", with: "mickeymouse@hyperneph.com"
-  			fill_in "Password", with: "Welcome100"
-  			fill_in "Confirmation", with: "Welcome100"
-  		end
+  		before { valid_signup_info }
 
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
@@ -55,7 +50,7 @@ describe "User pages" do
 
   			it { should have_link('Sign out') }
   			it { should have_title(user.name) }
-  			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  			it { should have_success_message('Welcome') }
   		end
   	end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,26 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def valid_signup_info
+  fill_in "Name", with: "Example User"
+  fill_in "Email", with: "mickeymouse@hyperneph.com"
+  fill_in "Password", with: "Welcome100"
+  fill_in "Confirmation", with: "Welcome100"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
