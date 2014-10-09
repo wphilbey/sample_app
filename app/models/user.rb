08c3739767c5e680001c58af67217846b10ba639
Warren Philbey <wphilbey@hyperneph.com>
2014-10-09 15:04:58 +0100

    Finish user edit, update index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, 
 			  length: { minimum: 6 }
+	validates :password_confirmation, presence: true
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Example User"
-    email    "user@hyperneph.com"
-    password "Welcome100"
-    password_confirmation "Welcome100"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
